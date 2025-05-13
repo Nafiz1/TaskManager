f60ee38c76a6086e86e4feb61fb02550efa97269
Nafiz <nafizh007@gmail.com>
2025-05-12 23:12:30 -0400

    updated connection string and migrated to taskdb2

--- obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5061a51385a58f705c48587ff550092b494bf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc66952f7b1fd9835f2f690129d0502688363ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
