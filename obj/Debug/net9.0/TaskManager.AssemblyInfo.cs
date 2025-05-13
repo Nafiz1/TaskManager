8bc66952f7b1fd9835f2f690129d0502688363ed
Nafiz <nafizh007@gmail.com>
2025-05-12 23:04:27 -0400

    update binary files

--- obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d18590123fb5d6ad32c964ff9d246a7101324e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5061a51385a58f705c48587ff550092b494bf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
